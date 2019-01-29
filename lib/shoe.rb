4155fd3cc72c6a63cb74a5f479c9ecde8561080b
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-29 17:52:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,10 @@
 class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
   def initialize(brand)
     @brand = brand 
   end
-  
-  attr_accessor :brand, :color, :size, :material, :condition
   
   def cobble 
     puts "Your shoe is as good as new!"
